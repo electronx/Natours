f41253ea5c66f810285b397fa621f4226c8afc1d
Zura <zurabiashvili1@gmail.com>
2022-05-27 19:58:04 +0400

    quering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -41,9 +41,25 @@ exports.getAllTours = async (req, res) => {
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
-      console.log(sortBy);
     } else {
       query = query.sort('-createdAt');
+    }
+
+    //4) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    }
+
+    //5) pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
     }
 
     //EXECUTE QUERY
