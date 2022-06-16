9e2ed16dab88f907621112e9b6704f9b19670920
zura iashvili <zurabiashvili1@gmail.com>
2022-06-16 23:18:26 +0400

    added stripe webhooks

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -58,7 +58,7 @@ exports.updateBooking = factory.updateOne(Booking);
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.data.object.amount_total / 100;
+  const price = session.amount_total / 100;
   console.log(tour, user, price);
   await Booking.create({ tour, user, price });
 };
